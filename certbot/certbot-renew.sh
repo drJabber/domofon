1f9812834523dc0caedffbf3c4ffc1bf82910dd1
drJabber@gmail.com <dJabber@gmail.com>
2021-01-23 23:37:04 +0300

    certbot manual with hook

--- certbot/certbot-renew.sh
+++ certbot/certbot-renew.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-certbot certonly --non-interactive --manual --preferred-challenges=dns --manual-auth-hook \
-     ./authenticator.sh --manual-cleanup-hook ./auth_cleanup.sh -d services.ozuevo.top
+certbot certonly --non-interactive --manual --preferred-challenges=dns \
+     --manual-auth-hook ${CERTBOT_SCRIPTS_PATH}/authenticator.sh \
+     --manual-cleanup-hook ${CERTBOT_SCRIPT_PATH}/auth_cleanup.sh \
+     -d ${CERTBOT_DOMAIN}
