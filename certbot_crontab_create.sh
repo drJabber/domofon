fe466f7545f30d7d19f4e2efc3d5bfd222b9b8fe
drJabber@gmail.com <dJabber@gmail.com>
2021-01-24 01:03:34 +0300

    for certbot crontab

--- certbot_crontab_create.sh
+++ certbot_crontab_create.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 APP_PATH=$(pwd)
-echo "0 6,22 * * * root sleep $[ ( $RANDOM % 60 )  + 1 ]m && APP_PATH=$APP_PATH $(grep -v '^#' $APP_PATH/.env.prod | xargs) $APP_PATH/certbot/certbot_renew.sh" > /etc/cron.d/certbot-renew
+echo "0 6,22 * * * root sleep \$[ ( \$RANDOM % 60 )  + 1 ]m && APP_PATH=$APP_PATH $(grep -v '^#' $APP_PATH/.env.prod | xargs) $APP_PATH/certbot/certbot_renew.sh" > /etc/cron.d/certbot-renew
